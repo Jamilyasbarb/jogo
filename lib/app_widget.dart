84a8809805b52dbbd03a08fddb4716f2e5f5a875
Jamilyasbarb <mily.yasbarb@gmail.com>
2023-02-23 09:55:01 -0300

    feat: theme data e rotas
    iniciando configuracao de themas comecando pela fonte, e configurada rotas da aplicacao

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:jogo/pages/config/config_page.dart';
+import 'package:jogo/pages/home/home_page.dart';
+import 'package:jogo/pages/jogo/jogo_page.dart';
 
 class AppWidet extends StatelessWidget {
   const AppWidet({super.key});
@@ -6,9 +9,14 @@ class AppWidet extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        fontFamily: 'Montserrat'
+      ),
       initialRoute: '/',
       routes: {
-        
+        '/': (context) => HomePage(),
+        'jogo': (context) => JogoPage(),
+        'config': (context) => ConfigPage(),
       },
     );
   }
