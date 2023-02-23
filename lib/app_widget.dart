3226a61ab437ba4d239f1dd5f2924dd2983fa28a
Jamilyasbarb <mily.yasbarb@gmail.com>
2023-02-23 17:18:37 -0300

    feat: add provider
    adicionado provider de tempo

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,23 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:jogo/controllers/timer_controller.dart';
 import 'package:jogo/pages/config/config_page.dart';
 import 'package:jogo/pages/home/home_page.dart';
 import 'package:jogo/pages/jogo/jogo_page.dart';
+import 'package:provider/provider.dart';
 
 class AppWidet extends StatelessWidget {
   const AppWidet({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        fontFamily: 'Montserrat'
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => TimerController(),)
+      ],
+      child: MaterialApp(
+        theme: ThemeData(
+          fontFamily: 'Montserrat'
+        ),
+        initialRoute: '/',
+        routes: {
+          '/': (context) => HomePage(),
+          'jogo': (context) => JogoPage(),
+          'config': (context) => ConfigPage(),
+        },
       ),
-      initialRoute: '/',
-      routes: {
-        '/': (context) => HomePage(),
-        'jogo': (context) => JogoPage(),
-        'config': (context) => ConfigPage(),
-      },
     );
   }
 }
