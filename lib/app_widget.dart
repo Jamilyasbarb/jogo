0e8bba69627ce6701614aee352a7af05a6c036a9
Jamilyasbarb <mily.yasbarb@gmail.com>
2023-02-23 08:14:25 -0300

    commit inicial

---
+++ lib/app_widget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AppWidet extends StatelessWidget {
+  const AppWidet({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: '/',
+      routes: {
+        
+      },
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:jogo/app_widget.dart';
+
+void main() {
+  runApp(const AppWidet());
+}
+
